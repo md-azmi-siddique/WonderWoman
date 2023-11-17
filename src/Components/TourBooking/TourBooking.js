d6244ef3e78a22b72c3350790f46772feb4faa77
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-11-17 19:08:05 +0600

    Tour Booking structure ready

--- src/Components/TourBooking/TourBooking.js
+++ src/Components/TourBooking/TourBooking.js
@@ -1,11 +1,31 @@
 import React from "react";
+import img1 from '../../Assets/TourBooking/img1.jpg'
+import img2 from '../../Assets/TourBooking/img2.jpg'
+import img3 from '../../Assets/TourBooking/img3.jpg'
+import img4 from '../../Assets/TourBooking/img4.jpg'
 
 const TourBooking = () => {
   return (
     <div>
       <div class="grid grid-cols-3 gap-4">
         <div class="col-span-2 ...">04</div>
-        <div class="...">05</div>
+        <div class="pr-16">
+          <div class="grid grid-cols-2 gap-4">
+            <div class="...">
+                <img src={img1} alt="" />
+            </div>
+            <div class="...">
+                <img src={img2} alt="" />
+            </div>
+            <div class="...">
+                <img src={img3} alt="" />
+            </div>
+            <div class="...">
+                <img src={img4} alt="" />
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
   );
